90fe64ffe34aee176ff6e5e83d5cb65c6f46ccc1
javierbull <javierbullreyes@gmail.com>
2022-03-24 10:03:23 -0500

    base

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+  // custom input file
+  $("form").on("change", ".file-upload-field", function(){
+      $(this).parent(".file-upload-wrapper").attr("data-text",
+      $(this).val().replace(/.*(\/|\\)/, '') );
+  });
+  // Fin custom input file
+
+});
