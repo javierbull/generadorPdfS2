1371c4743c0c8023894255d53c524395016bb676
javierbull <javierbullreyes@gmail.com>
2022-03-24 11:54:10 -0500

    input password

--- js/main.js
+++ js/main.js
@@ -7,4 +7,17 @@ $(document).ready(function() {
   });
   // Fin custom input file
 
+  // SHOW-HIDE PASSWORD
+  $(".toggle-password").click(function() {
+
+    $(this).toggleClass("fa-eye fa-eye-slash");
+    var input = $($(this).attr("toggle"));
+    if (input.attr("type") == "password") {
+      input.attr("type", "text");
+    } else {
+      input.attr("type", "password");
+    }
+  });
+  // END SHOW-HIDE PASSWORD
+
 });
